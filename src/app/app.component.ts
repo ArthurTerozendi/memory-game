6fbf4a1332ef432b4860eed2044849b64f45b49a
Arthur Terozendi <arthurterozendi@gmail.com>
2022-08-26 21:24:33 -0300

    set input info in card and adjust css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'memory-game';
+  cards = [
+    {
+      back: 'back 1',
+      front: 'front 1'
+    },
+    {
+      back: 'back 2',
+      front: 'front 2'
+    },
+    {
+      back: 'back 3',
+      front: 'front 3'
+    },
+  ];
 }
