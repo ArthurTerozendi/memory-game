74c8cc0f4311d195d529849f975a3a2cb845f84f
Arthur Terozendi <arthurterozendi@gmail.com>
2022-08-26 21:42:21 -0300

    flip on click using angular browser animation

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,18 +1,44 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  animations: [
+    trigger('flipState', [
+      state(
+        'active',
+        style({
+          transform: 'rotateY(180deg)',
+        })
+      ),
+      state(
+        'inactive',
+        style({
+          transform: 'rotateY(0)',
+        })
+      ),
+      transition('active => inactive', animate('400ms ease-out')),
+      transition('inactive => active', animate('400ms ease-in')),
+    ]),
+  ],
 })
 export class CardComponent implements OnInit {
-
   @Input() backsideContent = '';
   @Input() frontsideContent = '';
+  flippedCard = 'inactive';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  flipCard() {
+    this.flippedCard = this.flippedCard == 'inactive' ? 'active' : 'inactive';
   }
-
 }
